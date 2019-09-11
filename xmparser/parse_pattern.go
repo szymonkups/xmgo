65954ed8d61fe3a74bfa86fe266eaeebf3673638
Szymon <szymonkups@gmail.com>
2019-09-11 22:16:55 +0200

    Improve pattern parsing

--- xmparser/parse_pattern.go
+++ xmparser/parse_pattern.go
@@ -22,20 +22,17 @@ type Row = []Cell
 
 // ParsePatterns parses XM file patterns starting of given offset
 func parsePattern(f *os.File, fileHeader *XMFileHeader) (*Pattern, error) {
-	pattern := Pattern{}
-
 	header, err := parsePatternHeader(f)
 
 	if err != nil {
 		return nil, err
 	}
 
-	pattern.Header = *header
-	pattern.Rows = make([]Row, header.NoRows)
+	rows := make([]Row, header.NoRows)
 
 	if header.DataSize > 0 {
 		for r := uint16(0); r < header.NoRows; r++ {
-			pattern.Rows[r] = make([]Cell, fileHeader.NoChannels)
+			rows[r] = make([]Cell, fileHeader.NoChannels)
 			for c := uint16(0); c < fileHeader.NoChannels; c++ {
 				cell, err := parseCell(f)
 
@@ -43,12 +40,12 @@ func parsePattern(f *os.File, fileHeader *XMFileHeader) (*Pattern, error) {
 					return nil, err
 				}
 
-				pattern.Rows[r][c] = *cell
+				rows[r][c] = *cell
 			}
 		}
 	}
 
-	return &pattern, nil
+	return &Pattern{*header, rows}, nil
 }
 
 func parsePatternHeader(f *os.File) (*PatternHeader, error) {
